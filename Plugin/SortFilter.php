1b9eee1a297b1768b66a8ac7f3724c8e1c43482b
Frederik Rommel <frederik.rommel@webidea24.de>
2022-03-02 12:20:51 +0100

    Initial commit

---
+++ Plugin/SortFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Webidea24\SortedFilter\Plugin;
+
+use Magento\Catalog\Model\Layer\Filter\FilterInterface;
+
+class SortFilter
+{
+    /**
+     * @param FilterInterface $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetItems(FilterInterface $subject, array $result): array
+    {
+        $return = [];
+
+        foreach ($result as $filterItem) {
+            $return[$filterItem->getLabel()] = $filterItem;
+        }
+
+        ksort($return);
+
+        return $return;
+    }
+}
